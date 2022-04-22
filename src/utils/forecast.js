7c39c164e81db93b7ec39b2793cebff5d2815e47
Biggie <76620767+Masud-Ndatsu@users.noreply.github.com>
2022-04-22 10:15:36 +0100

    humidity data added to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,24 +1,20 @@
-const request = require('request');
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=2ea46cdfdca8427c9d7c03f2838670c7&query=${longitude},${latitude}&units=s`
+  const url = `http://api.weatherstack.com/current?access_key=2ea46cdfdca8427c9d7c03f2838670c7&query=${longitude},${latitude}&units=s`;
 
-    request({
-        url,
-        json: true
-    }, (error, {
-        body
-    }) => {
-        if (error) {
-            callback('Unable to connect to the weather service!', undefined);
-        } else if (body.error) {
-            callback('Unable to find location!', undefined)
-        } else {
-            callback(undefined, `Clear throughout the day. It is currently ${body.current.temperature}. The weather feels like ${body.current.feelslike} inside here.`)
-        }
-
-    })
-
-}
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to the weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location!", undefined);
+    } else {
+      callback(
+        undefined,
+        `Clear throughout the day. It is currently ${body.current.temperature}. The weather feels like ${body.current.feelslike} inside here. the humidity is ${body.current.humidity}.`
+      );
+    }
+  });
+};
 
 module.exports = forecast;
